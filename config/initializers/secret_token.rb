09ced6b7e80d821c0bacc1fbe54aa5718f889351
Manish Kumar <manish.kumar5@tcs.com>
2014-04-06 12:59:11 +0530

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChirperApp::Application.config.secret_token = '1f62a9af5b75e1c35fc46af49a99af6af437a89d065808bc2be47022aa656e7caa5434ec1a65a2c6b7052e99a8120182b21d2aea881fb6c5b1edbe8a20d0f89b'
